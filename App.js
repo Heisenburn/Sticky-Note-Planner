c8a9b6ce1c1f7e8d552146cbbbd5ef074d015910
Marcin Rudnik <marcinrudnik1996@gmail.com>
2022-01-26 22:08:53 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Alert,
+  Pressable,
+  Text,
+} from "react-native";
 
 export default function App() {
+  const [exampleState, setExampleState] = useState("defaultValue");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Pressable
+          style={styles.categoryButton}
+          onPress={() => Alert.alert("test")}
+        >
+          <Text style={styles.heading}>TYDZIEŃ</Text>
+          <Text style={[styles.numberOfElements, styles.boxShadow]}>23</Text>
+        </Pressable>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+  },
+  heading: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  boxShadow: {
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  numberOfElements: {
+    color: "white",
+    fontSize: 60,
+    fontWeight: "bold",
+    paddingTop: 20,
+    textAlign: "center",
+  },
+  categoryButton: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "orange",
+    borderRadius: 55,
+    borderWidth: 8,
+    borderColor: "white",
+    padding: 70,
   },
 });
