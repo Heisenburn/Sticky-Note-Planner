6637cde15b446873d4be1a8e3bbdb9e49394be32
Marcin Rudnik <marcinrudnik1996@gmail.com>
2022-10-09 14:37:20 +0200

    refactor listviewscreen part2

--- Views/ListViewScreen/Components/CustomSortableList.tsx
+++ Views/ListViewScreen/Components/CustomSortableList.tsx
@@ -1,17 +1,23 @@
-import {SortableList, Text, TouchableOpacity, View} from "react-native-ui-lib";
-import React, {useState} from "react";
-import {Item} from "../types";
-import styles from "../ListViewScreen.style";
-import {CustomCheckbox} from "./Checkbox";
-import {Entypo} from "@expo/vector-icons";
+import { SortableList, Text, TouchableOpacity, View } from 'react-native-ui-lib'
+import React, { useCallback, useState } from 'react'
+import { Item } from '../types'
+import styles from '../ListViewScreen.style'
+import { CustomCheckbox } from './Checkbox'
+import { Entypo } from '@expo/vector-icons'
+import * as Haptics from 'expo-haptics'
 
-export const CustomSortableList = ({data}) => {
+export const CustomSortableList = ({
+    data,
+    setIsListingItemOptionsModalVisible,
+    editedElement,
+    categoryId,
+}) => {
     const [listItems] = useState(data)
 
     const renderItem = ({
-                            item,
-                            index: _index,
-                        }: {
+        item,
+        index: _index,
+    }: {
         item: Item
         index: number
     }) => {
@@ -43,7 +49,26 @@ export const CustomSortableList = ({data}) => {
         )
     }
 
-    return(
+    const keyExtractor = useCallback((item: Item) => {
+        return `${categoryId}-${item.note}`
+    }, [])
+
+    const onOrderChange = useCallback(async (newData) => {
+        // const mappedData = rootData.map((item) => {
+        //     if (item.categoryId === categoryId) {
+        //         item.details.items = newData
+        //     }
+        //     return item
+        // })
+
+        // updateData(mappedData)
+
+        await Haptics.notificationAsync(
+            Haptics.NotificationFeedbackType.Success
+        )
+    }, [])
+
+    return (
         <SortableList
             data={listItems}
             renderItem={renderItem}
@@ -52,5 +77,4 @@ export const CustomSortableList = ({data}) => {
             scale={1.12}
         />
     )
-
 }
--- Views/ListViewScreen/ListViewScreen.tsx
+++ Views/ListViewScreen/ListViewScreen.tsx
@@ -1,44 +1,16 @@
-// //TODO: może użyć tego?
-// // const getColor = (i) => {
-// //     const multiplier = 255 / (NUM_ITEMS - 1)
-// //     const colorVal = i * multiplier
-// //     return `rgb(${colorVal}, ${Math.abs(128 - colorVal)}, ${255 - colorVal})`
-// // }
-
-// import { MaterialIcons } from '@expo/vector-icons'
-import styles from './ListViewScreen.style'
-import * as Haptics from 'expo-haptics'
 import FloatingButton from '../../Shared/FloatingButton/FloatingButton'
 import { Dimensions } from 'react-native'
-import React, {useCallback, useState, useContext, useRef, useMemo} from 'react'
-import {
-    SortableList,
-    View,
-    TouchableOpacity,
-    Text,
-    ActionSheet,
-} from 'react-native-ui-lib'
-
-import { CategoriesWithNotesContext } from '../../Context/CategoriesWithNotesContext'
-import { Entypo } from '@expo/vector-icons'
-import { CustomCheckbox } from './Components/Checkbox'
+import React, { useState, useRef, useMemo } from 'react'
+import { View, ActionSheet } from 'react-native-ui-lib'
 import { ACTIONS } from '../../Shared/constants'
-import {Item} from "./types";
-import {CustomSortableList} from "./Components/CustomSortableList";
-
+import type { Item } from './types'
+import { CustomSortableList } from './Components/CustomSortableList'
 
 const ListViewScreen = ({ route, navigation }) => {
     const { passedPropsFromPreviousScreen } = route.params
     const { categoryItem } = passedPropsFromPreviousScreen
     const { categoryId, details } = categoryItem
     const { items: data, categoryTitle } = details || []
-
-
-
-
-    const { getData, updateData } = useContext(CategoriesWithNotesContext)
-    const rootData = getData();
-
 
     const editedElement = useRef<Item | null>(null)
 
@@ -68,26 +40,6 @@ const ListViewScreen = ({ route, navigation }) => {
         setIsListingItemOptionsModalVisible,
     ] = useState(false)
 
-    const keyExtractor = useCallback((item: Item) => {
-        return `${categoryId}-${item.note}`
-    }, [])
-
-    const onOrderChange = useCallback(async (newData) => {
-
-        const mappedData = rootData.map((item) => {
-            if (item.categoryId === categoryId) {
-                item.details.items = newData
-            }
-            return item
-        })
-
-        updateData(mappedData)
-
-        await Haptics.notificationAsync(
-            Haptics.NotificationFeedbackType.Success
-        )
-    }, [])
-
     // const handleSettingsClick = useCallback(() => {
     //     navigation.navigate('SettingsScreen', {
     //         passedPropsFromPreviousScreen: {
@@ -99,12 +51,7 @@ const ListViewScreen = ({ route, navigation }) => {
     //     })
     // }, [])
 
-
-
-
-
-    const MIN_HEIGHT = useMemo((()=>Dimensions.get('window').height - 100), []);
-
+    const MIN_HEIGHT = useMemo(() => Dimensions.get('window').height - 100, [])
 
     return (
         <View useSafeArea style={{ minHeight: MIN_HEIGHT, marginTop: 10 }}>
@@ -121,7 +68,14 @@ const ListViewScreen = ({ route, navigation }) => {
             {/*        onPress={handleSettingsClick}*/}
             {/*    />*/}
             {/*</View>*/}
-            <CustomSortableList data={data}/>
+            <CustomSortableList
+                data={data}
+                setIsListingItemOptionsModalVisible={
+                    setIsListingItemOptionsModalVisible
+                }
+                editedElement={editedElement}
+                categoryId={categoryId}
+            />
             <ActionSheet
                 title={'Opcje'}
                 message={'Wybierz właściwą akcje'}
