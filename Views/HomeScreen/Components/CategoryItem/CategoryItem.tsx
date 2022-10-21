e835f02ef7a35d1e9f0dc6ba7d5e94c929000956
Marcin Rudnik <marcinrudnik1996@gmail.com>
2022-10-21 14:30:22 +0200

    refactor the view of homePage

--- Views/HomeScreen/Components/CategoryItem/CategoryItem.tsx
+++ Views/HomeScreen/Components/CategoryItem/CategoryItem.tsx
@@ -1,6 +1,7 @@
 import { Pressable, Text } from 'react-native'
 import { CategoryWithNotesType } from '../../../../types/types'
 import styles from '../../HomeScreen.styles'
+import { View } from 'react-native-ui-lib'
 
 const CategoryItem = ({
     item,
@@ -13,11 +14,7 @@ const CategoryItem = ({
 
     return (
         <Pressable
-            style={[
-                styles.categoryButton,
-                styles.boxShadow,
-                { backgroundColor: 'orange' },
-            ]}
+            style={[styles.categoryButton, styles.boxShadow]}
             onPress={() => {
                 navigation.navigate('ListViewScreen', {
                     passedPropsFromPreviousScreen: {
@@ -28,9 +25,19 @@ const CategoryItem = ({
             }}
         >
             <Text style={styles.heading}>{categoryTitle}</Text>
-            <Text style={[styles.numberOfElements, styles.boxShadow]}>
-                {items?.length}
-            </Text>
+            <View
+                style={{
+                    borderRadius: 20,
+                    backgroundColor: '#2F80ED',
+                    height: 60,
+                    width: 60,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Text style={[styles.numberOfElements]}>{items.length}</Text>
+            </View>
         </Pressable>
     )
 }
