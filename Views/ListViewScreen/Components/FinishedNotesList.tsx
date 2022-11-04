257e86180445041fc28aa51068c0993e31d65be9
Marcin Rudnik <marcinrudnik1996@gmail.com>
2022-11-04 16:31:15 +0100

    add logic for restoring and removing finished notes

--- Views/ListViewScreen/Components/FinishedNotesList.tsx
+++ Views/ListViewScreen/Components/FinishedNotesList.tsx
@@ -1,6 +1,6 @@
 import { FlatList } from 'react-native'
 import { ItemInCategoryType } from '../../../types/types'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {
     Button,
     ExpandableSection,
@@ -9,64 +9,122 @@ import {
     View,
 } from 'react-native-ui-lib'
 import { Entypo } from '@expo/vector-icons'
+import { CategoriesWithNotesContext } from '../../../Context/CategoriesWithNotesContext'
 
 export const FinishedNotesList = ({ data }: { data: ItemInCategoryType[] }) => {
     const [isFinishedNotesListingVisible, setIsFinishedNotesListingVisible] =
         useState(false)
 
-    const renderItem = ({ item }: { item: ItemInCategoryType }) => (
-        <TouchableOpacity
-            style={{
-                backgroundColor: 'white',
-                borderBottomWidth: 1,
-                borderColor: '#d4d4d4bf',
-                minHeight: 60,
-            }}
-            centerV
-            paddingH-10
-        >
-            <View flex row spread centerV>
-                <View
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        width: '100%',
-                    }}
-                >
-                    <Text
-                        center
-                        $textDefault
-                        style={{
-                            maxWidth: 200,
-                            textDecorationLine: 'line-through',
-                        }}
-                    >
-                        {item.note}
-                    </Text>
+    const { getData, updateData } = useContext(CategoriesWithNotesContext)
+    const rootData = getData()
+
+    const renderItem = ({ item }: { item: ItemInCategoryType }) => {
+        const handleRestoringNote = () => {
+            const dataWithRestoredElement = rootData.map((categoryItem) => {
+                if (categoryItem.details.items.includes(item)) {
+                    const itemsWithChangedCheckbox =
+                        categoryItem.details.items.map((note) => {
+                            if (note.id === item.id) {
+                                return {
+                                    ...note,
+                                    checked: false,
+                                }
+                            }
+                            return note
+                        })
+
+                    return {
+                        ...categoryItem,
+                        details: {
+                            ...categoryItem.details,
+                            items: itemsWithChangedCheckbox,
+                        },
+                    }
+                }
+
+                return categoryItem
+            })
+
+            updateData(dataWithRestoredElement)
+        }
+
+        const handleRemoveNote = () => {
+            const dataWithoutElement = rootData.map((categoryItem) => {
+                if (categoryItem.details.items.includes(item)) {
+                    const itemsWithoutElement =
+                        categoryItem.details.items.filter(
+                            (note) => note.id !== item.id
+                        )
+
+                    return {
+                        ...categoryItem,
+                        details: {
+                            ...categoryItem.details,
+                            items: itemsWithoutElement,
+                        },
+                    }
+                }
+                return categoryItem
+            })
+
+            updateData(dataWithoutElement)
+        }
+
+        return (
+            <TouchableOpacity
+                style={{
+                    backgroundColor: 'white',
+                    borderBottomWidth: 1,
+                    borderColor: '#d4d4d4bf',
+                    minHeight: 60,
+                }}
+                centerV
+                paddingH-10
+            >
+                <View flex row spread centerV>
                     <View
                         style={{
                             display: 'flex',
                             flexDirection: 'row',
-                            alignSelf: 'flex-end',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            width: '100%',
                         }}
                     >
-                        <Button>
-                            <Text>Przywróc</Text>
-                        </Button>
-                        <Button
+                        <Text
+                            center
+                            $textDefault
                             style={{
-                                backgroundColor: 'red',
+                                maxWidth: 200,
+                                textDecorationLine: 'line-through',
                             }}
                         >
-                            <Text>Usuń</Text>
-                        </Button>
+                            {item.note}
+                        </Text>
+                        <View
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignSelf: 'flex-end',
+                            }}
+                        >
+                            <Button onPress={() => handleRestoringNote()}>
+                                <Text>Przywróc</Text>
+                            </Button>
+                            <Button
+                                style={{
+                                    backgroundColor: 'red',
+                                }}
+                                onPress={() => handleRemoveNote()}
+                            >
+                                <Text>Usuń</Text>
+                            </Button>
+                        </View>
                     </View>
                 </View>
-            </View>
-        </TouchableOpacity>
-    )
+            </TouchableOpacity>
+        )
+    }
 
     return (
         <>
